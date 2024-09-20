df690f73c7971f9bf23a909f8e13832c5eaec7d8
Dimas Rosyidin <dimasrosyidinn@gmail.com>
2024-09-20 08:32:29 +0700

    Pertemuan 4 - Praktikum 1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,23 +11,29 @@ class UserController extends Controller
     public function index(){
       // tambah data user dengan Eloquent Model
       
-        // $data = [
-        //     'username' => 'customer-1',
-        //     'nama'  => 'Pelanggan',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 4
-        //  ];
-        // UserModel::insert($data); //tambahkan data ke tabel m_user
-
         $data = [
-            'nama'  => 'Pelanggan Pertama',
-        ];
-        UserModel::where('username', 'customer-1')->update($data); //update data user
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama'  => 'Manager 3',
+            'password' => Hash::make('12345'),
+            
+         ];
+        UserModel::create($data); //tambahkan data ke tabel m_user
+
+
+         //coba akses model UserModel
+         $user = UserModel::all(); //Ambil semua data dari tabel m_user
+         return view('user', ['data' => $user]);
+
+
+
+        // $data = [
+        //     'nama'  => 'Pelanggan Pertama',
+        // ];
+        // UserModel::where('username', 'customer-1')->update($data); //update data user
        
 
-      //coba akses model UserModel
-        $user = UserModel::all(); //Ambil semua data dari tabel m_user
-        return view('user', ['data' => $user]);
+     
     
 
 
