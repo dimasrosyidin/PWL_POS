135ca9643e58d982b76219e849406c244db8be7d
Dimas Rosyidin <dimasrosyidinn@gmail.com>
2024-09-23 00:04:10 +0700

    Pertemuan 4 - Praktikum 2.7

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,21 +10,68 @@ class UserController extends Controller
 {
     public function index(){
 
-      $user = UserModel::create([
-        'username' => 'manager11',
-        'nama' => 'Managers11',
-        'password' => Hash::make('12345'),
-        'level_id' => 2,
-    ]);
-    $user->username = 'manager12';
-    $user->save();
+      $user = UserModel::all(); //Ambil semua data dari tabel m_user
+      return view('user', ['data' => $user]);
+    }
+
+    public function tambah(){
+      return view('user_tambah');
+    }
+
+    public function tambah_simpan(Request $request) {
+      UserModel::create([
+          'username' => $request->username,
+          'nama' => $request->nama,
+          'password' => Hash::make($request->password),
+          'level_id' => $request->level_id,
+      ]);
+     
+      return redirect('/user');
+  }
+
+    public function ubah($id) {
+      $user = UserModel::find($id);
+      return view('user_ubah', ['data' => $user]);
+
+}
+
+    public function ubah_simpan($id, Request $request) {
+      $user = UserModel::find($id);
+      $user->username = $request->username;
+      $user->nama = $request->nama;
+      $user->password = Hash::make('$request->password');
+      $user->level_id = $request->level_id;
+
+      $user->save();
+    }
+
+
+  public function hapus($id) {
+  $user = UserModel::find($id);
+  $user->delete();
+  return redirect('/user');
+  
+}
+}
+
+
+
+
+    //   $user = UserModel::create([
+    //     'username' => 'manager11',
+    //     'nama' => 'Managers11',
+    //     'password' => Hash::make('12345'),
+    //     'level_id' => 2,
+    // ]);
+    // $user->username = 'manager12';
+    // $user->save();
     
-    return view('user', ['data' => $user]);
-    $user->wasChanged(); // true
-    $user->wasChanged('username'); // true
-    $user->wasChanged(['username', 'level_id']); // true
-    $user->wasChanged('nama'); // false
-    dd($user->wasChanged(['nama', 'username'])); //true
+    // return view('user', ['data' => $user]);
+    // $user->wasChanged(); // true
+    // $user->wasChanged('username'); // true
+    // $user->wasChanged(['username', 'level_id']); // true
+    // $user->wasChanged('nama'); // false
+    // dd($user->wasChanged(['nama', 'username'])); //true
 
       // $user = UserModel::Create(
       //   [
@@ -86,5 +133,3 @@ class UserController extends Controller
         // ];
         // UserModel::where('username', 'customer-1')->update($data); //update data user
        
-    }
-}
