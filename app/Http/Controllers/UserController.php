d62f91a84c7ce367a4282a5f89d1a211899b69e9
Dimas Rosyidin <dimasrosyidinn@gmail.com>
2024-09-28 22:47:58 +0700

    Pertemuan 5 - Praktikum 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,13 +20,22 @@ public function index()
         'title' => 'Daftar user yang terdaftar dalam sistem',
     ];
     $activeMenu = 'user'; // set menu yang sedang aktif
-    return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
+
+    $level = LevelModel::all(); //ambil data level untuk filter level
+    
+    return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level, 'activeMenu' => $activeMenu]);
   }
 
   public function list(Request $request)
   {
       $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
           ->with('level');
+
+      
+       // Filter data user berdasarkan level_id
+       if ($request->level_id) {
+        $users->where('level_id', $request->level_id);
+    } 
       return DataTables::of($users)
           // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
           ->addIndexColumn()
