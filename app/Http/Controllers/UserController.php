2b0641a5a7035d035cbce3290cc6acf5df6509c6
Dimas Rosyidin <dimasrosyidinn@gmail.com>
2024-10-04 23:53:13 +0700

    Pertemuan 6 - Praktikum 3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -142,6 +142,32 @@ public function index()
        return redirect('/');
    }
 
+    // Menampilkan halaman confirm hapus
+    public function confirm_ajax(string $id) {
+        $user = UserModel::find($id);
+        return view('user.confirm_ajax', ['user' => $user]);
+    }
+    // Menghapus data user dengan AJAX
+    public function delete_ajax(Request $request, $id) {
+        //cek apakah request dari ajax
+        if($request->ajax() || $request->wantsJson()) {
+            $user = UserModel::find($id);
+            if($user) {
+                $user->delete();
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Data berhasil dihapus'
+                ]);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Data tidak ditemukan'
+                ]);
+            }
+        }
+        return redirect('/');
+    }
+
   // Menampilkan halaman form tambah user
     public function create() {
       $breadcrumb = (object) [
