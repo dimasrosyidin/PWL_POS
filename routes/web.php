0c278065872e8c9edf72b356970a610e78506038
Dimas Rosyidin <dimasrosyidinn@gmail.com>
2024-10-11 09:51:05 +0700

    Pertemuan 7 - Praktikum 2 dan tugas 2

--- routes/web.php
+++ routes/web.php
@@ -61,7 +61,8 @@ Route::group(['prefix' => 'user'], function() {
 
 
 //route level
-Route::group(['prefix' =>'level'],function(){
+// Route::group(['prefix' =>'level'],function(){
+    Route::group(['prefix' =>'level', 'middleware' => 'authorize:ADM'],function(){
     Route::get('/', [LevelController::class, 'index']);          // menampilkan halaman awal level
     Route::post('/list', [LevelController::class, 'list']);      // menampilkan data level dalam json untuk datables
     Route::get('/create', [LevelController::class, 'create']);   // menampilkan halaman form tambah level
