1e0ae47c3781a721b891384abc91071bb25f8dd6
Dimas Rosyidin <dimasrosyidinn@gmail.com>
2024-11-05 17:02:57 +0700

    Pertemuan 10 - Praktikum 4

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Exceptions\JWTException;
 class LoginController extends Controller
 {
     public function __invoke(Request $request){
